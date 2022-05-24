4b492cb102333636f6250f385ae0e948d3e454ce
ys0222 <ggdbstjd0222@gmail.com>
2022-05-25 01:02:28 +0900

    0525

--- pay.js
+++ pay.js
@@ -1,6 +1,6 @@
 //결제하기 
 
-let payBtn =[`<button onclick='pay(price)'>결제하기</button>`]
+let payBtn =[`<button class='payBtn' onclick='pay(price)'>결제하기</button>`]
 
 pay = (price) => {
     alert(`결제하실 금액은 ${price}`)
